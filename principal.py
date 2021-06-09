2fe85f5ca70a51267693002d70bbd3763e64a09b
Alfredo <alfredomv@al.insper.edu.br>
2021-06-09 09:40:04 -0300

    embaralhar

--- principal.py
+++ principal.py
@@ -1,12 +1,15 @@
+from funcoes import embaralhar
 import pygame
 import random
 pygame.init()
 
-largura = 940
+largura = 938
 altura = 620
 diametro = 5
 
 white = (255, 255, 255)
+green = (0, 255, 0)
+green_table = (10, 75, 10)
 black = (0, 0, 0)
 
 window = pygame.display.set_mode((largura, altura))
@@ -20,11 +23,21 @@ img_da_carta = {}
 valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 naipes = ['deespadas', 'decopas', 'deouros', 'depaus']
 
+baralho = []
 for valor in valores:
     for naipe in naipes:
-        carta_auxiliar = valor + naipe
-        img_da_carta[carta_auxiliar] = [pygame.image.load(f"imgs\{carta_auxiliar}.jpg"), (random.randint(10, largura), random.randint(10, altura))]
-print (img_da_carta)
+        baralho.append(valor + naipe)
+print(baralho)
+random.shuffle(baralho)
+j = 0
+for ii in range(len(baralho)):
+    carta_auxiliar = baralho[ii]
+    i = ii % 13
+    if i == 0:
+        j += 1
+    img_da_carta[carta_auxiliar] = [pygame.image.load(f"imgs/{carta_auxiliar}.jpg"), ((i + 1) * int(largura / 14), (j) * int(largura / 9))]
+
+#print (img_da_carta)
 centro = pygame.Vector2(largura / 2, 0)
 
 continua = True
@@ -36,7 +49,7 @@ while continua:
             if event.key == pygame.K_ESCAPE:
                 continua = False
     # Para que serve os vetores?
-    window.fill((0, 0, 0))
+    window.fill(green_table)
 
     # Comando para mostrar na tela
     for valor_e_naipe_da_carta in img_da_carta:
